1e559cdc5bd9873cea0beba125158d2845b4e745
M0lli2 <bsg8247@gmail.com>
2025-03-24 10:42:54 +0900

    init : report start

---
+++ src/main/java/com/example/calenderreport/CalenderReportApplication.java
@@ -0,0 +1,13 @@
+package com.example.calenderreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalenderReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalenderReportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calenderreport/CalenderReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calenderreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalenderReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
